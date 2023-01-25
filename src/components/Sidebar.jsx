47e9c05179d9a42c29b33e8739ad0897d9c5e04c
leticea <nascimento.leticeah@gmail.com>
2023-01-25 08:42:49 -0300

    editando sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,15 @@
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <aside id="sidebar">
+      <p>imagem</p>
+      <p className="title">Desenvolvedora</p>
+      <p>redes sociais</p>
+      <p>informações de contato</p>
+      <a href="" className="btn">
+        Download currículo
+      </a>
+    </aside>
+  );
 };
 
 export default Sidebar;
