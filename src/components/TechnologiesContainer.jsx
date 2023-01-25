0c8c571fd08f12ad54a16c3542238363111f0271
leticea <nascimento.leticeah@gmail.com>
2023-01-25 11:39:07 -0300

    editando container

--- src/components/TechnologiesContainer.jsx
+++ src/components/TechnologiesContainer.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import {
+  DiHtml5,
+  DiCss3,
+  DiJsBadge,
+  DiNodejsSmall,
+  DiReact,
+  DiMysql,
+  DiPhp,
+  DiLaravel,
+} from "react-icons/di";
+
+import "../styles/components/technologiescontainer.sass";
+
+const technologies = [
+  { id: "html", name: "HTML5", icon: <DiHtml5 /> },
+  { id: "css", name: "CSS3", icon: <DiCss3 /> },
+  { id: "js", name: "JavaScript", icon: <DiJsBadge /> },
+  { id: "node", name: "Node.js", icon: <DiNodejsSmall /> },
+  { id: "react", name: "React", icon: <DiReact /> },
+  { id: "mysql", name: "MySQL", icon: <DiMysql /> },
+  { id: "php", name: "PHP", icon: <DiPhp /> },
+  { id: "laravel", name: "Laravel", icon: <DiLaravel /> },
+];
 
 const TechnologiesContainer = () => {
-  return <div>TechnologiesContainer</div>;
+  return (
+    <section className="technologies-container">
+      <h2>Tecnologias</h2>
+      <div className="technologies-grid">
+        {technologies.map((tech) => (
+          <div className="technology-card" id={tech.id} key={tech.id}>
+            {tech.icon}
+            <div className="technology-info">
+              <h3>{tech.name}</h3>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default TechnologiesContainer;
